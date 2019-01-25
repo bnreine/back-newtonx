c1ccc8a196581f96a5530b570b782abc7de3d903
Benjamin <bnreinecke0209@gmail.com>
2019-01-25 00:26:49 -0500

    updates

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,10 +1,6 @@
 module.exports = {
   init(app){
-    //const staticRoutes = require("../routes/static");
-    //const listRoutes = require("../routes/lists");
     const userRoutes = require("../routes/users");
-    //app.use(staticRoutes);
-    //app.use(listRoutes);
     app.use(userRoutes);
   }
 }
